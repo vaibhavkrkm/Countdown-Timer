5e219744cc252e493c80c08ae9026b9f763ecabb
Vaibhav Kumar <vaibhavkrkm@gmail.com>
2021-02-19 21:56:34 +0530

    Added Stopwatch

--- main.c
+++ main.c
@@ -20,12 +20,14 @@ TIMER_TIME convertTime(int);
 int convertIntoSec(TIMER_TIME);
 void clearScreen(void);
 // UI functions
-void startMenu(void);
+int startMenu(void);
 void alarmMenu(void);
 
 void main()
 {
+	short int mode;
 	int startingSec, sec;
+	char keyPress;
 	TIMER_TIME convertedTime, remainingTime, userTime;
 	
 	alarm_fp = fopen("alarm_tone.data", "r");
@@ -36,65 +38,154 @@ void main()
 		fscanf(alarm_fp, "%s", &selected_alarm_tone);
 		fclose(alarm_fp);
 	}
-	
-	startMenu();
-	
-	while(1)
+		
+	LOOP:
 	{
-		while(1)
+		mode = startMenu();
+		
+		if(mode == 1)
 		{
-			// clearing the screen
-			clearScreen();
-			
-			// taking user input
-			printf("Enter time in the following format:\nHOUR(S) MINUTE(S) SECONDS(S)\nFor Start Menu, enter -1 -1 -1\n");
-			fflush(stdin);
-			scanf("%d %d %d", &userTime.hr, &userTime.min, &userTime.sec);
-			
-			if(userTime.hr < 0 || userTime.min < 0 || userTime.sec < 0)
+			while(1)
 			{
-				startMenu();
-				continue;
+				while(1)
+				{
+					// clearing the screen
+					clearScreen();
+					
+					// taking user input
+					printf("Enter time in the following format:\nHOUR(S) MINUTE(S) SECONDS(S)\nFor Start Menu, enter -1 -1 -1\n");
+					fflush(stdin);
+					scanf("%d %d %d", &userTime.hr, &userTime.min, &userTime.sec);
+					
+					if(userTime.hr < 0 || userTime.min < 0 || userTime.sec < 0)
+					{
+						goto LOOP;
+					}
+					else
+						break;
+				}
+				sec = convertIntoSec(userTime);
+				
+				convertedTime = convertTime(sec);
+				
+				for(startingSec=sec; startingSec>0; startingSec--)
+				{
+					// clearing the screen
+					clearScreen();
+					
+					// displaying messages
+					timerMessage(convertedTime);
+					remainingTime = convertTime(startingSec);
+					printf("%d : %d : %d", remainingTime.hr, remainingTime.min, remainingTime.sec);
+					
+					// waiting for one second...
+					waitFor(1000.0);
+				}
+				
+				// playing the time out audio
+				PlaySound(TEXT(selected_alarm_tone), NULL, SND_ASYNC);
+				
+				// clearing the screen
+				clearScreen();
+				
+				//displaying messages
+				timerMessage(convertedTime);
+				printf("Time is Up!\n");
+				printf("Press any key to continue...");
+				
+				getch();
+				PlaySound(NULL, NULL, SND_ASYNC);
 			}
-			else
-				break;
 		}
-		sec = convertIntoSec(userTime);
-		
-		convertedTime = convertTime(sec);
-		
-		for(startingSec=sec; startingSec>0; startingSec--)
+		else
 		{
-			// clearing the screen
-			clearScreen();
+			startingSec = 0;
 			
-			// displaying messages
-			timerMessage(convertedTime);
-			remainingTime = convertTime(startingSec);
-			printf("%d : %d : %d", remainingTime.hr, remainingTime.min, remainingTime.sec);
-			
-			// waiting for one second...
-			waitFor(1000.0);
+			while(1)
+			{
+				// clearing the screen
+				clearScreen();
+				
+				// waiting for user input for starting the stopwatch
+				remainingTime = convertTime(startingSec);
+				printf("Stopwatch [Status: STOPPED]\n%d : %d : %d\n", remainingTime.hr, remainingTime.min, remainingTime.sec);
+				puts("Press any key to start (Press S for Start Menu)");
+				
+				keyPress = getch();
+				if(keyPress == 's' || keyPress == 'S')
+					goto LOOP;
+				
+				startingSec = 0;
+				
+				while(1)
+				{
+					// checking key presses
+					if(kbhit())
+					{
+						keyPress = getch();
+						
+						if(keyPress == ' ')      // stop stopwatch
+						{
+							break;
+						}
+						else if(keyPress == 'p' || keyPress == 'P')      // pause stopwatch
+						{
+							// clearing the screen
+							clearScreen();
+							
+							// displaying messages
+							printf("Stopwatch [Status: PAUSED]\n%d : %d : %d\n", remainingTime.hr, remainingTime.min, remainingTime.sec);
+							puts("Press any key to resume the stopwatch...");
+							
+							waitFor(800.0);
+							
+							while(1)
+							{
+								if(kbhit())
+									break;
+								
+								// clearing the screen
+								clearScreen();
+								
+								waitFor(500.0);
+								
+								// displaying the messages
+								printf("Stopwatch [Status: PAUSED]\n%d : %d : %d\n", remainingTime.hr, remainingTime.min, remainingTime.sec);
+								puts("Press any key to resume the stopwatch...");
+								
+								waitFor(800.0);
+							}
+						}
+					}
+					
+					// clearing the screen
+					clearScreen();
+					
+					// displaying messages
+					remainingTime = convertTime(startingSec);
+					printf("Stopwatch [Status: RUNNING]\n%d : %d : %d\n", remainingTime.hr, remainingTime.min, remainingTime.sec);
+					puts("Press P to pause or S to stop the stopwatch.");
+					
+					// updating startingSec
+					startingSec++;
+					
+					// waiting for one second...
+					waitFor(1000.0);
+				}
+				
+				/* // clearing the screen
+				clearScreen();
+				
+				// displaying messages
+				printf("Stopwatch [Status: STOPPED]\n%d : %d : %d\n", remainingTime.hr, remainingTime.min, remainingTime.sec);
+				puts("Press any key to start (Press P for Start Menu)"); */
+			}
 		}
-		
-		// playing the time out audio
-		PlaySound(TEXT(selected_alarm_tone), NULL, SND_ASYNC);
-		
-		// clearing the screen
-		clearScreen();
-		
-		//displaying messages
-		timerMessage(convertedTime);
-		printf("Time is Up!\n");
-		printf("Press any key to continue...");
-		
-		getch();
-		PlaySound(NULL, NULL, SND_ASYNC);
 	}
 }
 
-// start menu function
-void startMenu()
+// start menu function     (1 - TIMER, 2 - STOPWATCH)
+int startMenu()
 {
 	short int userInput;
 	
@@ -105,18 +196,22 @@ void startMenu()
 		
 		// taking user input
 		printf("Hey there! Welcome to Countdown Timer!\nPlease type an option number and press Enter\n");
-		printf("1) Set Timer\n2) Change Tone\n3) Quit\n");
+		printf("1) Set Timer\n2) Run Stopwatch\n3) Change Tone\n4) Quit\n");
 		scanf("%d", &userInput);
 		
 		if(userInput == 1)
 		{
-			return;
+			return 1;
 		}
 		else if(userInput == 2)
 		{
-			alarmMenu();
+			return 2;
 		}
 		else if(userInput == 3)
+		{
+			alarmMenu();
+		}
+		else if(userInput == 4)
 		{
 			exit(0);
 		}
